faa9100398341a6eba0b47de45d3c26d3ce934a3
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-06-08 16:38:00 +0200

    visdom: init at 0.1.8.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3173,6 +3173,8 @@ in
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
+  visdom = callPackage ../applications/misc/visdom { };
+
   volctl = callPackage ../tools/audio/volctl { };
 
   vorta = python3Packages.callPackage ../applications/backup/vorta { };
