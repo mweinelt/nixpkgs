06c41b3349622d0c596fd0052b9eca52c20a89b8
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-05-08 01:04:25 +0200

    python3Packages.pushbullet: add missing test dependency cryptography

--- pkgs/development/python-modules/pushbullet/default.nix
+++ pkgs/development/python-modules/pushbullet/default.nix
@@ -1,6 +1,13 @@
-{ lib, buildPythonPackage, fetchPypi
-, requests, websocket_client, python_magic
-, pytest, mock }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, python_magic
+, requests
+, websocket_client
+, cryptography
+, mock
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "pushbullet.py";
@@ -11,13 +18,28 @@ buildPythonPackage rec {
     sha256 = "917883e1af4a0c979ce46076b391e0243eb8fe0a81c086544bcfa10f53e5ae64";
   };
 
-  propagatedBuildInputs = [ requests websocket_client python_magic ];
+  propagatedBuildInputs = [
+    python_magic
+    requests
+    websocket_client
+  ];
 
-  checkInputs = [ pytest mock ];
+  checkInputs = [
+    cryptography
+    mock
+    pytestCheckHook
+  ];
 
-  checkPhase = ''
-    PUSHBULLET_API_KEY="" py.test -k "not test_e2e and not test_auth"
+  preCheck = ''
+    export PUSHBULLET_API_KEY=""
   '';
+
+  disabledTests = [
+    # these tests require network access
+    "test_auth_fail"
+    "test_auth_success"
+    "test_decryption"
+  ];
 
   meta = with lib; {
     description = "A simple python client for pushbullet.com";
