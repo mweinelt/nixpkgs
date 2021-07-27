86a80fd851a54e86952185d2e1f514952c3c8918
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-07-27 15:13:24 +0200

    nixos/evcc: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -489,6 +489,7 @@
   ./services/hardware/vdr.nix
   ./services/home-automation/home-assistant.nix
   ./services/home-automation/zigbee2mqtt.nix
+  ./services/home-automation/evcc.nix
   ./services/logging/SystemdJournal2Gelf.nix
   ./services/logging/awstats.nix
   ./services/logging/filebeat.nix
