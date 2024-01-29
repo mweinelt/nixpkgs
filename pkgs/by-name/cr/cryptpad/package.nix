f244c18be57763c71b4c10bce77bd0b5f9cfdec5
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-01-30 00:03:56 +0100

    cryptpad: init at 5.6.0
    Cryptpad is a realtime collaborative visual editor backed by a zero
    knowlege server.

--- pkgs/by-name/cr/cryptpad/package.nix
+++ pkgs/by-name/cr/cryptpad/package.nix
@@ -21,7 +21,12 @@ buildNpmPackage {
 
   npmDepsHash = "sha256-tQUsI5Oz3rkAlxJ1LpolJNqZfKUGKUYSgtuCTzHRcW4=";
 
-  npmFlagsArray = [
-    "--loglevel=verbose"
-  ];
+  meta = with lib; {
+    changelog = "https://github.com/cryptpad/cryptpad/releases/tag/${version}";
+    description = "Collaborative office suite, end-to-end encrypted and open-source";
+    downloadPage = "https://github.com/cryptpad/cryptpad";
+    homepage = "https://cryptpad.org";
+    license = licenses.agpl3Plus;
+    maintainers = with maintainers; [ hexa ];
+  };
 }
