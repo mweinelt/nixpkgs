20f1c9fa23904aae062523e5a8bf22ec380b12e5
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-05-13 16:28:07 +0200

    lego: 4.35.2 -> 5.0.3

--- pkgs/by-name/le/lego/package.nix
+++ pkgs/by-name/le/lego/package.nix
@@ -7,20 +7,18 @@
 
 buildGoModule (finalAttrs: {
   pname = "lego";
-  version = "4.35.2";
+  version = "5.0.3";
 
   src = fetchFromGitHub {
     owner = "go-acme";
     repo = "lego";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-NBCvVlMDEEhlfWWG7X5T1Udg+42+ibS1Ph6F+/yrXF0=";
+    hash = "sha256-nhmbHk2extAuJcVmu86vBzAv4+tLjpK/R47QwnBn50I=";
   };
 
-  vendorHash = "sha256-Q85McGGSILE8BPwreCtih6my1nih9ameLKHFe1dgNWQ=";
+  vendorHash = "sha256-//UVZHEYjMHZFkm8H49KIhpQATRTJatveOnQzI6L0uw=";
 
   doCheck = false;
-
-  subPackages = [ "cmd/lego" ];
 
   ldflags = [
     "-s"
